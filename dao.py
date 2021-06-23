079a2c4ac66248dbff102bd32bde416492d0db13
Hongju Lee <litehj84@gmail.com>
2021-06-23 17:40:27 +0900

    Update dao.py
    로그인 로직, 게시판 불러오기, 게시글 불러오기 위한 코드 수정 및 추가

--- dao.py
+++ dao.py
@@ -4,7 +4,7 @@ import json
 import collections
 
 class MemberDAO:
-    def memid(self):  
+    def memid(self, user_pw):  
 
         data = ''
         try:
@@ -12,7 +12,9 @@ class MemberDAO:
             cur = conn.cursor()
 
             try:
-                cur.execute("select mid from member")
+                cur.execute("select mid from member where mpw=:mpw", mpw=user_pw)
+                row = cur.fetchone()
+                data = row[0]
 
             except Exception as e:
                 print(e) 
@@ -26,24 +28,6 @@ class MemberDAO:
 
         return data
 
-    def meminsert(self, dto):
-        # insert into emp01 values (1234, 'test', 200);
-        conn = cx_Oracle.connect(user="MYMATH2", password="1234", dsn="xe")
-        cur = conn.cursor()
-
-        try:
-            cur.execute("insert into members values (:empno, :ename, :sal)", \
-                empno=dto.getEmpno(), ename=dto.getEname(), sal=dto.getSal()) 
-
-            conn.commit()
-        except Exception as e:
-            print(e) 
-
-        finally:
-            cur.close() 
-            conn.close()
-
-
 
 class QuestionDAO:
     def questall(self):  
@@ -54,15 +38,16 @@ class QuestionDAO:
             cur = conn.cursor()
 
             try:
-                cur.execute("select qsid, title, mname from questions, members where questions.memno=members.memno") 
+                cur.execute("select qsid, grade, title, mname from q_table, member where q_table.memno=member.memno") 
                 rows = cur.fetchall()
 
                 v = []
                 for row in rows:
                     d = collections.OrderedDict()
                     d['qsid'] = row[0]
-                    d['title'] = row[1]
-                    d['mname'] = row[2]
+                    d['grade'] = row[1]
+                    d['title'] = row[2]
+                    d['mname'] = row[3]
                     v.append(d)
 
                 data = json.dumps(v, ensure_ascii = False)
@@ -79,17 +64,19 @@ class QuestionDAO:
 
         return data
 
+
+
     def questone(self, qsid):  
 
         data = ''
         try:
-            conn = cx_Oracle.connect(user="SCOTT", password="TIGER", dsn="xe")
+            conn = cx_Oracle.connect(user="MYMATH2", password="1234", dsn="xe")
             cur = conn.cursor()
 
             try:
-                cur.execute("select * from emp01 where empno=:v", v=empno) 
+                cur.execute("select title, content, mname from q_table, member where q_table.memno=member.memno and qsid=:qsid", qsid=qsid) 
                 row = cur.fetchone() 
-                data = '{"ename":"' + row[1] + '", "sal":' + str(row[2]) +'}'
+                data = '{"title":"' + row[0] + '", "content":"' + row[1] + '", "mname":"' + row[2] + '"}'
 
             except Exception as e:
                 print(e) 
@@ -102,3 +89,28 @@ class QuestionDAO:
             conn.close()
 
         return data
+
+
+    def questinsert(self, dto):
+
+        conn = cx_Oracle.connect(user="MYMATH2", password="1234", dsn="xe")
+        cur = conn.cursor()
+
+        try:
+            cur.execute("insert into q_table values (:qsid, :grade, :title, :memno, :content)", \
+                empno=dto.getEmpno(), ename=dto.getEname(), sal=dto.getSal()) 
+            conn.commit()
+        except Exception as e:
+            print(e) 
+
+        finally:
+            cur.close() 
+            conn.close()
+
+
+
+
+# if __name__ == "__main__":
+#     dao = MemberDAO()
+#     dao.memid()
+
